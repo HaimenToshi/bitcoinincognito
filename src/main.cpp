5ed7e2e5f7e1ebaf33f9962cd244b720107b2064
Daniel Wilson <r3mapper@gmail.com>
2018-07-03 01:59:22 +0100

    Correct bug - code did not build

--- src/main.cpp
+++ src/main.cpp
@@ -107,7 +107,7 @@ static void CheckBlockIndex();
 /** Constant stuff for coinbase transactions we create: */
 CScript COINBASE_FLAGS;
 
-const string strMessageMagic = "DarkNet Signed Message:\n
+const string strMessageMagic = "DarkNet Signed Message:\n";
 
 // Internal stuff
 namespace
